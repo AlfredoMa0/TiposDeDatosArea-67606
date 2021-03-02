c8b0325e5b520f38c4ef35cd5405f76d769cb4f7
Campe <Campe@LAPTOP-F5OP52OL>
2021-03-01 22:24:44 -0600

    AREA

---
+++ src/tipodedatosarea/pkg67606/TipoDeDatosArea67606.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatosarea.pkg67606;
+
+/**
+ *
+ * @author Campe
+ */
+public class TipoDeDatosArea67606 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        int base= 10;
+        int altura =5;
+        int Resultado;
+        Resultado= base*altura;
+        
+        System.out.println("LA BASE ES: " + base);
+        System.out.println("LA ALTURA ES: " + altura);
+        System.out.println("El RESULTADO ES = " + Resultado);
+        
+        
+        
+        
+       
+        
+        
+    }
+    
+}
